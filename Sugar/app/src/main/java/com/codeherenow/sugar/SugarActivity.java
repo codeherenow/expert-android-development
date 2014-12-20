c7c9fa1f92a79f26e04d42b70983b9f428c1ae9d
Ragunath Jawahar <rj@mobsandgeeks.com>
2014-12-20 23:56:51 +0530

    [Example] Sugar, initial commit.

---
+++ Sugar/app/src/main/java/com/codeherenow/sugar/SugarActivity.java
@@ -0,0 +1,14 @@
+package com.codeherenow.sugar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SugarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sugar);
+    }
+
+}
