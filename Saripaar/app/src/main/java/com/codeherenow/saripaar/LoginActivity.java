769251b364ab8b900ef58e83bb708bbbbf424f05
Ragunath Jawahar <rj@mobsandgeeks.com>
2014-12-20 23:50:43 +0530

    [Example] Saripaar, initial commit.

---
+++ Saripaar/app/src/main/java/com/codeherenow/saripaar/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.codeherenow.saripaar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
